41879e2c4dcd91c955f73a2927ece15a4362a3fc
vmartinyan <vahram.martinyan@hotmail.com>
2016-12-20 23:26:00 +0400

    asd

--- wp-config.php
+++ wp-config.php
@@ -18,6 +18,8 @@
  * @package WordPress
  */
 
+//test
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 /* define('DB_NAME', 'u858866342_equby'); */
